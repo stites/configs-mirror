06a6aa7f478d3ed9479554830c3485e2f3f70849
Sam Stites <stites@users.noreply.github.com>
2019-10-20 01:57:28 -0400

    bump starship version

--- overlays/starship.nix
+++ overlays/starship.nix
@@ -1,4 +1,12 @@
 self: super:
+let
+  starship-25-pr = super.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "f21be15a84d81eabc6d06e1cea093cb8894aced1";
+    sha256 = "0dffy3y6x6an6y2s99rp5a53x6jw3paxx2w1fcfgkj94fnbl3cx1";
+  };
+in
 {
-  starship = (import <unstable> {}).starship;
+  starship = (import starship-25-pr {}).starship;
 }
