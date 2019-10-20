a8d41cabe9eb85b9a97ed48d1610b8d2420d7f0c
Sam Stites <stites@users.noreply.github.com>
2019-10-20 01:56:48 -0400

    adjust starship configs to account for broken git status

--- experiments.nix
+++ experiments.nix
@@ -5,7 +5,7 @@
     enableBashIntegration = true;
     settings = {
       # Don't print a new line at the start of the prompt
-      "add_newline" = false;
+      add_newline = false;
 
       # Replace the "❯" symbol in the prompt with "➜"
       character = {  # The name of the module we are configuring is "character"
@@ -24,14 +24,30 @@
       };
 
       line_break = {
-        disabled = true;
+        disabled = false; # until the git-status is fixed
       };
 
       git_branch = {
         prefix = "";
-        symbol = "🍂";
-        truncation_length = "4";
+        symbol = "";
+        truncation_length = 10;
       };
     };
   };
 }
+
+
+# [git_status]
+# conflicted = "x"
+# ahead = "^"
+# behind = "v"
+# diverged = "%"
+# untracked = "u"
+# stashed = "s"
+# modified = "m"
+# staged = "*"
+# renamed = "r"
+# deleted = "_"
+# # prefix = "["  # Prefix to display immediately before git status.
+# # suffix = "]"  # Suffix to display immediately after git status.
+# style = "bold red" # The style for the module.
