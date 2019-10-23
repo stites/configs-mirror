08e28b491836b7c04a3114f5524ed266b627a5fd
Sam Stites <stites@users.noreply.github.com>
2019-10-23 12:10:32 -0400

    install mypython for neovim-lsp

--- programs/python/default.nix
+++ programs/python/default.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 let
-  install-python = false;
+  install-python = true;
 in
 {
   imports = [
@@ -8,66 +8,73 @@ in
     ./ipython.nix
   ];
   home.packages = lib.optionals install-python [
-      (let
-        dontCheck = pypkg: pypkg.overridePythonAttrs (old: {
-          doCheck = false; checkPhase = "true";
-        });
+    (let
+      dontCheck = pypkg: pypkg.overridePythonAttrs (old: {
+        doCheck = false; checkPhase = "true";
+      });
 
-        jupyter = import (builtins.fetchGit {
-          url = https://github.com/tweag/jupyterWith;
-          rev = "02b929122d49189a896c9fae044c12db21846f25";
-        }) {};
+      jupyter = import (builtins.fetchGit {
+        url = https://github.com/tweag/jupyterWith;
+        rev = "02b929122d49189a896c9fae044c12db21846f25";
+      }) {};
 
-        jupyterEnvironment =
-          jupyter.jupyterlabWith { kernels = [
-            (jupyter.kernels.iPythonWith {
-              name = "python";
-              packages = p: with p; [ numpy
-                nose (dontCheck celery) mock flake8 pygments          # testing
-                hypothesis pytest pytest-mypy             # testing
-                numpy # compute storage
-                beautifulsoup4 requests                         # web processing
-                # pandas # numba (dontCheck pyarrow) h5py                       # compute storage
-                ipython notebook ipywidgets jupyter             # notebooks
-                matplotlib seaborn tensorflow-tensorboard       # visualization
-                scikitlearn scipy                               # statistics and ML
-                pillow (dontCheck imageio) # pycv (not in nixpkgs) # CV
-                nltk gensim                                     # NLP
-                # # pymc3            # <<< TOTALLY BROKEN           # PPLs
-                # pytorch-world.pytorch # mypytorch mytorchvision probtorch pyro-ppl    # future
-              ];
-            })
-            (jupyter.kernels.iHaskellWith {
-              name = "haskell";
-              packages = p: with p; [ hvega formatting ];
-            })
-          ]; };
+      jupyterEnvironment =
+        jupyter.jupyterlabWith { kernels = [
+          (jupyter.kernels.iPythonWith {
+            name = "python";
+            packages = p: with p; [ numpy
+              nose (dontCheck celery) mock flake8 pygments          # testing
+              hypothesis pytest pytest-mypy             # testing
+              numpy # compute storage
+              beautifulsoup4 requests                         # web processing
+              # pandas # numba (dontCheck pyarrow) h5py                       # compute storage
+              ipython notebook ipywidgets jupyter             # notebooks
+              matplotlib seaborn tensorflow-tensorboard       # visualization
+              scikitlearn scipy                               # statistics and ML
+              pillow (dontCheck imageio) # pycv (not in nixpkgs) # CV
+              nltk gensim                                     # NLP
+              # # pymc3            # <<< TOTALLY BROKEN           # PPLs
+              # pytorch-world.pytorch # mypytorch mytorchvision probtorch pyro-ppl    # future
+            ];
+          })
+          (jupyter.kernels.iHaskellWith {
+            name = "haskell";
+            packages = p: with p; [ hvega formatting ];
+          })
+        ]; };
 
-        python36 = pkgs.python36.override {
-          packageOverrides = pself: psuper: {
-            matplotlib  = dontCheck (psuper.matplotlib.override { enableQt = true; });
-            scipy = dontCheck psuper.scipy;
-            beautifulsoup4 = dontCheck psuper.beautifulsoup4;
-          };
-          self = python36;
+      python36 = pkgs.python36.override {
+        packageOverrides = pself: psuper: {
+          matplotlib  = dontCheck (psuper.matplotlib.override { enableQt = true; });
+          scipy = dontCheck psuper.scipy;
+          beautifulsoup4 = dontCheck psuper.beautifulsoup4;
         };
+        self = python36;
+      };
 
-        # mypython36 = python36.withPackages (ps: with ps; [
-        #   # nose celery mock flake8 pygments     # testing
-        #   hypothesis pytest pytest-mypy                             # testing
-        #   numpy  # compute storage
-        # ] ++ map dontCheck [
-        #   beautifulsoup4 requests                         # web processing
-        #   pandas numba pyarrow h5py                       # compute storage
-        #   ipython notebook ipywidgets jupyter             # notebooks
-        #   matplotlib seaborn tensorflow-tensorboard       # visualization
-        #   scikitlearn scipy                               # statistics and ML
-        #   pillow imageio # pycv (not in nixpkgs) # CV
-        #   nltk gensim                                     # NLP
-        #   # # pymc3            # <<< TOTALLY BROKEN           # PPLs
-        #   # pytorch-world.pytorch # mypytorch mytorchvision probtorch pyro-ppl    # future
-        # ]);
-      in (pkgs.stdenv.mkDerivation {
+      mypython36 = python36.withPackages (ps: with ps; [
+        # dev packages
+        mccabe mypy nose pycodestyle pydocstyle
+        jedi flake8 pygments pytest-mypy pyls-isort pyls-mypy pyflakes yapf
+        typeguard
+
+        beautifulsoup4 requests                         # web processing
+        ipython notebook ipywidgets jupyter             # notebooks
+        pandas numba pyarrow h5py numpy                 # compute storage
+        matplotlib seaborn tensorflow-tensorboard       # visualization
+
+        nose celery mock flake8 pygments     # testing
+        hypothesis pytest pytest-mypy        # testing
+
+      ] ++ map dontCheck [
+        # scikitlearn scipy                               # statistics and ML
+        # pillow imageio # pycv (not in nixpkgs) # CV
+        # nltk gensim                                     # NLP
+        # # pymc3            # <<< TOTALLY BROKEN           # PPLs
+        # pytorch-world.pytorch # mypytorch mytorchvision probtorch pyro-ppl    # future
+
+      ]);
+      justExes = pkgs.stdenv.mkDerivation {
         name = "my-python-executables";
         buildPhase = "";
         # buildInputs = [ mypython36 pkgs.makeWrapper ];
@@ -86,8 +93,7 @@ in
             # "${wrapPyBin "jupyter-notebook" "nb"  ""}"
             # "${wrapPyBin "tensorboard"      "tb"  ""}"
           ];
-      }))
-    ];
-
-
+      };
+    in mypython36)
+  ];
 }
