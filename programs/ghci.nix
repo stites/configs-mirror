9a7920211b1227605de9d555702c9413c7f8745f
Sam Stites <stites@users.noreply.github.com>
2019-10-23 12:10:11 -0400

    more useful ghci

--- programs/ghci.nix
+++ programs/ghci.nix
@@ -20,15 +20,16 @@
     )
     ":set -cpp -DASSERTS -DDEBUG"
     ":set -Wno-name-shadowing"
-    # ":set -XOverloadedStrings -XScopedTypeVariables -XTupleSections -XFlexibleContexts -XDataKinds"
+    # ":set -XOverloadedStrings -XScopedTypeVariables -XFlexibleContexts -XDataKinds"
+    ":set -XNumericUnderscores -XTupleSections"
 
     # -- :set +s
-    # -- :set -XPartialTypeSignatures
+    ":set -XPartialTypeSignatures"
 
     # -- import qualified Data.Text   as T
     # -- import qualified Data.Vector as V
     # -- import qualified Data.HashSet as HS
     # -- import qualified Data.HashMap.Strict as HM
-    # -- import Data.Monoid ((<>))
+    "import Data.Monoid ((<>))"
   ];
 }
