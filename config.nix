0e20a889b6d55bcb32a1d6bc8c8650aa097ab7f8
Sam Stites <stites@users.noreply.github.com>
2019-10-30 12:58:29 -0400

    add unstable as an import to config

--- config.nix
+++ config.nix
@@ -115,6 +115,7 @@ in
 
   packageOverrides = pkgs_: (with pkgs_; {
     stable = import <nixos> { inherit config; };
+    unstable = import <unstable> { inherit config; };
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { pkgs = pkgs_; };
     inherit cuda9-shell cuda10-shell cuda-shell;
     inherit clang7-shell clang-shell;
